e3f1e7a504e54edbb375661420040f8c442a9bfe
saadat-neosoft <saadat.badgujar@neosoftmail.com>
2023-05-24 16:18:39 +0530

    completed signup section

--- src/layout/HomeSectionSignUp.jsx
+++ src/layout/HomeSectionSignUp.jsx
@@ -1,13 +1,27 @@
 import "../styles/components/signupSection.scss";
+import rightArrow from "../assets/right-arrow.svg";
 
 const HomeSectionSignUp = () => {
     return (
         <div className="signup-container">
-            <div className="signup-container__title">
-                SIGN UP FOR NEWS & OFFERS!
-            </div>
-            <div className="signup-container__description">
-                Subscribe to the weekly newsletter for all the latest updates
+            <div className="signup-container__container">
+                <div className="signup-container__title">
+                    SIGN UP FOR NEWS & OFFERS!
+                </div>
+                <div className="signup-container__description">
+                    Subscribe to the weekly newsletter for all the latest
+                    updates
+                </div>
+                <form className="signup-container__form">
+                    <input
+                        className="signup-container__form__input"
+                        type="text"
+                        placeholder="Enter your email"
+                    />
+                    <button className="signup-container__form__btn">
+                        <img src={rightArrow} alt="" />
+                    </button>
+                </form>
             </div>
             <div className="gradient-layer"></div>
         </div>
