c57d7683d75aa9949b8998df8e9c07f8dd0c3b4b
saadat-neosoft <saadat.badgujar@neosoftmail.com>
2023-05-31 10:42:11 +0530

    changed class to className

--- src/components/login/HeroRight.jsx
+++ src/components/login/HeroRight.jsx
@@ -2,39 +2,43 @@ import { Link } from "react-router-dom";
 
 const HeroRight = () => {
     return (
-        <div class="hero__right">
-            <form action="/login-form" method="post" class="hero__right__form">
-                <span class="hero__right__form-header">LOGIN</span>
+        <div className="hero__right">
+            <form
+                action="/login-form"
+                method="post"
+                className="hero__right__form"
+            >
+                <span className="hero__right__form-header">LOGIN</span>
                 <label for="loginEmail" class="hero__right__form-label">
                     Email
                 </label>
                 <input
                     type="email"
                     id="loginEmail"
-                    class="hero__right__form-control"
+                    className="hero__right__form-control"
                     placeholder="youremail@address.com"
                 />
-                <label for="loginEmail" class="hero__right__form-label">
+                <label for="loginEmail" className="hero__right__form-label">
                     Password
                 </label>
                 <input
                     type="password"
                     id="loginPassword"
-                    class="hero__right__form-control"
+                    className="hero__right__form-control"
                     placeholder="••••••••••••"
                 />
                 <Link
                     to="/forgot-password"
-                    class="hero__right__form__forgot-password"
+                    className="hero__right__form__forgot-password"
                 >
                     Forgot password
                 </Link>
-                <button type="submit" class="hero__right__form-btn">
+                <button type="submit" className="hero__right__form-btn">
                     Continue
                 </button>
-                <span class="hero__right__form-signup">
+                <span className="hero__right__form-signup">
                     New to NeoSTORE?
-                    <Link to="/signup" class="hero__right__form-signup-btn">
+                    <Link to="/signup" className="hero__right__form-signup-btn">
                         Sign up
                     </Link>
                 </span>
